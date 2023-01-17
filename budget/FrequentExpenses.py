acc7b344d01173ed5a00d47203167c3758737df4
Iris Sun <irissun@live.com>
2023-01-16 23:57:11 -0500

    Sec. 1 Step 7 - Plot the Top 5 Most Common Categories

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,5 +1,6 @@
 from . import Expense
 import collections
+import matplotlib.pyplot as plt
 
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
@@ -11,3 +12,4 @@ for expense in expenses.list:
 spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
 categories, count = zip(*top5)
+fig, ax = plt.subplots()
