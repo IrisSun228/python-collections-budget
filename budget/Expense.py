48405ed5482be4dbd9b6a9034ada913a1cb0a1d3
Iris Sun <irissun@live.com>
2023-01-17 09:52:09 -0500

    Sec. 4 Step 4 - Categorize unnecessary expenses

--- budget/Expense.py
+++ budget/Expense.py
@@ -52,3 +52,5 @@ class Expenses():
         food_expenses = {x for x in self.list if
                          x.category == 'Groceries' or
                          x.category == 'Eating Out'}
+
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
