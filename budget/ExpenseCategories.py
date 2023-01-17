8bf183d8d5429fb467856203a406a7b12cc3ae66
Iris Sun <irissun@live.com>
2023-01-17 10:12:00 -0500

    Sec. 5 Step 8 - Create the list of sums

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -8,6 +8,8 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = expenses.categorize_set_comprehension()
+    divided_expenses_sum = []
+
     if divided_set_comp != divided_for_loop:
         print('Sets are NOT equal by == test')
 
